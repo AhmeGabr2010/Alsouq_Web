3799e2cfb3f581672fa37d97f4339070fd8f84f9
AhmeGabr2010 <a.gabr26284@gmail.com>
2024-11-25 20:08:33 +0200

    Fix empty page issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3062600145"
+    serviceWorkerVersion: "1177971847"
   }
 });
